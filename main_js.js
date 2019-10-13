cfacda165a07927e7e87fa191b64230e89674c5e
MinhNC <45997670+MinhNCnextgen@users.noreply.github.com>
2019-10-13 18:05:29 +0700

    ádsad
    ádsad

--- main_js.js
+++ main_js.js
@@ -18,19 +18,19 @@ var span = document.getElementsByClassName("close")[0];
 function showInfo(type) {
     modal.style.display = "block";
     if (type==1){
-        document.getElementById('p_img').src='img/Minh 2.jpg'
+        document.getElementById('p_img').src='img/minh.jpg'
         document.getElementById('p_txt1').textContent='1.Nguyễn Công Minh'
         document.getElementById('p_txt2').textContent='Là Học sinh lớp 8 trường Ngô Sĩ Liên'
         document.getElementById('p_txt3').textContent='Sở thích:Chơi game, Nghe nhạc.'
         document.getElementById('p_txt4').textContent='Đảm nhận công việc:Lập trình trang chủ cho web'
     }else if(type==2){
-        document.getElementById('p_img').src='img/Uyên 2.jpg'
+        document.getElementById('p_img').src='img/uyen.jpg'
         document.getElementById('p_txt1').textContent='2.Phương Thi Uyên'
         document.getElementById('p_txt2').textContent='Là học sinh lớp 12 trường THPT Việt Đức'
         document.getElementById('p_txt3').textContent='Sở thích:Đọc truyện, nghe nhạc, xem phim'
         document.getElementById('p_txt4').textContent='Đảm nhận công việc:Nghĩ ra tên web, lên ý tưởng, thuyết trình,viết bài marketing'
     }else if(type==3){
-        document.getElementById('p_img').src='img/Chi 2.jpg'
+        document.getElementById('p_img').src='img/chi.jpg'
         document.getElementById('p_txt1').textContent='3.Ngô Mai Chi'
         document.getElementById('p_txt2').textContent='Là học sinh lớp 11 trường Nguyễn Tất Thành'
         document.getElementById('p_txt3').textContent='Sở thích:Nghe nhạc, makeup, design,..'
